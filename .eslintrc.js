e516d32ff0ddc9ad7a7b7e9eea47dcb19edc96de
zeroyu <zero@zerosrat.com>
2018-05-25 17:30:58 +0800

    editorconfig & eslint 配置 Please enter the commit message for your changes. Lines starting

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+export default {
+  parser: 'babel-eslint',
+  'plugins': [
+    'flowtype'
+  ],
+  extends: [
+    'standard',
+    'standard-jsx',
+    'plugin:flowtype/recommended'
+  ],
+  env: {
+    browser: true,
+    node: true
+  },
+  rules: {
+    'space-before-function-paren': ['error', {
+      anonymous: 'never',
+      named: 'never',
+      asyncArrow: 'always'
+    }],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'jsx-quotes': ['error', 'prefer-double']
+  }
+};
