0f4b7f44ca6ac0f2cd19ea86be0159a3c46f0932
zeroyu <zero@zerosrat.com>
2018-06-15 21:22:43 +0800

    feat(project): init header of search project screen.

--- src/views/home/ProjectSearchScreen.js
+++ src/views/home/ProjectSearchScreen.js
@@ -1,13 +1,66 @@
 import React from 'react'
-import { StyleSheet, View, ScrollView, Text } from 'react-native'
+import { StyleSheet, View, ScrollView, Text, TouchableOpacity } from 'react-native'
+import TextInput from '../../components/common/TextInput'
+import { Feather } from '@expo/vector-icons'
 
 import { APP_BACKGROUD_COLOR } from '../../styles/common'
+import { px2dp, px2sp } from '../../utils/device'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: APP_BACKGROUD_COLOR
+  },
+
+  search: {
+    flex: 1,
+    borderRadius: 5,
+    overflow: 'hidden'
+  },
+  searchInput: {
+    paddingLeft: px2dp(30),
+    height: px2dp(60),
+    backgroundColor: '#fff',
+    fontSize: px2sp(28),
+    color: '#333'
+  },
+  headerRightText: {
+    // marginLeft: px2dp(16),
+    // marginRight: px2dp(32),
+    backgroundColor: 'transparent',
+    color: '#fff',
+    fontSize: px2sp(28)
+  }
+})
+
+class HeaderSearch extends React.Component {
+  render() {
+    const searchIptProps = {
+      underlineColorAndroid: 'transparent',
+      placeholder: '搜索'
+    }
+    return (
+      <View style={styles.search}>
+        <TextInput {...searchIptProps} style={styles.searchInput} />
+      </View>
+    )
+  }
+}
 
 export default class ProjectSearch extends React.Component {
   static navigationOptions = {
+    headerTitle: <HeaderSearch />,
+    headerRight: (
+      <View style={{ flex: 1, alignItems: 'center', width: 56 }}>
+        <TouchableOpacity onPress={() => alert('This is a button!')}>
+          <Text style={styles.headerRightText}><Feather name="filter" />&nbsp;筛选</Text>
+        </TouchableOpacity>
+      </View>
+    ),
     headerStyle: {
-      backgroundColor: 'linear-gradient(top bottom, white, black)'
-    }
+      backgroundColor: '#8d9caa'
+    },
+    headerTintColor: '#fff'
   }
 
   render() {
@@ -20,10 +73,3 @@ export default class ProjectSearch extends React.Component {
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: APP_BACKGROUD_COLOR
-  }
-})
