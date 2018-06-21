551bc4e3408456e7655d897bf3089f2b691524b8
zeroyu <zero@zerosrat.com>
2018-06-21 10:16:49 +0800

    refactor(search): 封装搜索header的两个组件；搜索input增加下右内边距以避开删除btn

--- src/views/search/ProjectSearchScreen.js
+++ src/views/search/ProjectSearchScreen.js
@@ -1,155 +1,26 @@
 import React from 'react'
-import { StyleSheet, View, ScrollView, Text, TouchableOpacity, Alert } from 'react-native'
-import TextInput from '../../components/common/TextInput'
-import { Feather } from '@expo/vector-icons'
+import { StyleSheet, View, ScrollView, Text } from 'react-native'
 
 import { APP_BACKGROUD_COLOR } from '../../styles/common'
 import { px2dp, px2sp } from '../../utils/device'
 
+import HeaderTitleSearch from './components/HeaderTitleSearch'
+import HeaderRightFilter from './components/HeaderRightFilter'
 import ProjectItem from './components/ProjectItem'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: APP_BACKGROUD_COLOR
-  },
-
-  search: {
-    flex: 1,
-    borderRadius: 5,
-    overflow: 'hidden'
-  },
-  searchInput: {
-    paddingLeft: px2dp(30),
-    height: px2dp(60),
-    backgroundColor: '#fff',
-    fontSize: px2sp(28),
-    color: '#333'
-  },
-  deleteIconContainer: {
-    position: 'absolute',
-    top: px2dp(15),
-    right: px2dp(20),
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: px2dp(30),
-    height: px2dp(30),
-    borderRadius: px2dp(30 / 2),
-    backgroundColor: '#8d9caa'
-  },
-  deleteIcon: {
-    flex: 1,
-    textAlign: 'center',
-    lineHeight: px2dp(30),
-    color: '#fff'
-  },
-  headerRightText: {
-    // marginLeft: px2dp(16),
-    // marginRight: px2dp(32),
-    backgroundColor: 'transparent',
-    color: '#fff',
-    fontSize: px2sp(28)
-  },
-
-  resultTextContainer: {
-    marginTop: px2dp(30),
-    marginBottom: 1,
-    paddingVertical: px2dp(22),
-    paddingLeft: px2dp(30),
-    backgroundColor: '#fff'
-  },
-  resultText: {
-    color: '#8f9ba7',
-    fontSize: px2sp(28)
-  },
-  resultTextHighlight: {
-    color: '#1dbbae',
-    fontSize: px2sp(30)
-  }
-})
-
-class HeaderTitleSearch extends React.Component {
-  state = {
-    searchIptVal: '',
-    showDeleteIcon: false
-  }
-  textInput = React.createRef()
-
-  searchProjects() {
-    const search = this.state.searchIptVal.trim()
-    if (search) {
-      Alert.alert(this.state.searchIptVal)
-    }
-  }
-
-  setSearchIptVal(val) {
-    this.setState({ searchIptVal: val })
-  }
-  setShowDeleteIcon(isShow) {
-    this.setState({ showDeleteIcon: isShow })
-  }
-
-  handleSubmit = () => {
-    this.searchProjects()
-  }
-  handleIptFocus = () => {
-    this.setShowDeleteIcon(true)
-  }
-  handleIptBlur = () => {
-    this.setShowDeleteIcon(false)
-  }
-  handleDeletePress = () => {
-    this.textInput.current.clear()
-    this.setSearchIptVal('')
-  }
-
-  render() {
-    const searchIptProps = {
-      underlineColorAndroid: 'transparent',
-      placeholder: '搜索'
-    }
-    return (
-      <View style={styles.search}>
-        <TextInput inputRef={this.textInput} {...searchIptProps} style={styles.searchInput}
-          onSubmitEditing={this.handleSubmit}
-          value={this.state.searchIptVal} onChangeText={(text) => this.setSearchIptVal(text)}
-          onFocus={this.handleIptFocus} onBlur={this.handleIptBlur} />
-        {this.state.showDeleteIcon &&
-          <View style={styles.deleteIconContainer}>
-            <TouchableOpacity onPress={this.handleDeletePress}>
-              <Text style={styles.deleteIcon}>×</Text>
-            </TouchableOpacity>
-          </View>}
-      </View>
-    )
-  }
-}
-
-class HeaderRightBtn extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', width: 56 }}>
-        <TouchableOpacity onPress={() => alert('This is a button!')}>
-          <Text style={styles.headerRightText}><Feather name="filter" />&nbsp;筛选</Text>
-        </TouchableOpacity>
-      </View>
-    )
-  }
-}
-
 export default class ProjectSearch extends React.Component {
-  state = {
-    searchIptVal: '',
-    projects: [{}, {}]
-  }
-
   static navigationOptions = {
     headerTitle: <HeaderTitleSearch />,
-    headerRight: <HeaderRightBtn />,
+    headerRight: <HeaderRightFilter />,
     headerStyle: {
       backgroundColor: '#8d9caa'
     },
     headerTintColor: '#fff'
+  }
+
+  state = {
+    searchIptVal: '',
+    projects: [{}, {}]
   }
 
   render() {
@@ -169,3 +40,26 @@ export default class ProjectSearch extends React.Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: APP_BACKGROUD_COLOR
+  },
+
+  resultTextContainer: {
+    marginTop: px2dp(30),
+    marginBottom: 1,
+    paddingVertical: px2dp(22),
+    paddingLeft: px2dp(30),
+    backgroundColor: '#fff'
+  },
+  resultText: {
+    color: '#8f9ba7',
+    fontSize: px2sp(28)
+  },
+  resultTextHighlight: {
+    color: '#1dbbae',
+    fontSize: px2sp(30)
+  }
+})
