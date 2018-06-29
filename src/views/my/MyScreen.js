1ed2913864a55b9b2a49ba46c0b6c1757c87ac1c
letch <953847229@qq.com>
2018-06-29 09:49:05 +0800

    feat(my)：我的header

--- src/views/my/MyScreen.js
+++ src/views/my/MyScreen.js
@@ -1,9 +1,16 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, Text, View, Button, Image, ImageBackground } from 'react-native'
+import { px2dp, px2sp } from 'src/utils/device'
+import { Ionicons } from '@expo/vector-icons'
+
 import { withNavigation } from 'react-navigation'
 
 @withNavigation
 export default class App extends React.Component {
+  static navigationOptions = {
+    header: null
+  }
+
   handleLoginPress = () => {
     this.props.navigation.navigate('Login')
   }
@@ -11,13 +18,75 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>我的</Text>
-        <Button onPress={this.handleLoginPress} title="login" />
+        <ImageBackground source={require('./background.png')} style={styles.bgImg}>
+          <View style={styles.infoContainer} >
+            <View style={styles.avatarContainer} >
+              <Image source={require('src/img/avatar1.png')} style={styles.avatar} />
+            </View>
+            <View style={styles.infoLineOne} >
+              <Text style={styles.infoText}>贾玲美</Text>
+              <Ionicons name="ios-female" size={16} color="#f63771" style={styles.sexIcon} />
+            </View>
+            <Text style={[styles.infoText, styles.infoLineTwo]}>电子科技大学 | 美术学院 | 教授</Text>
+            <View style={styles.infoLineThree} >
+              <Image source={require('src/img/certificate.png')} style={styles.confirmIcon} />
+              <Text style={styles.confirm}>已认证</Text>
+            </View>
+          </View>
+          {/* <Button onPress={this.handleLoginPress} title="login" /> */}
+        </ImageBackground>
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {},
+  bgImg: {
+    // width: px2dp(750),
+  },
+  infoContainer: {
+    alignItems: 'center',
+    paddingBottom: px2dp(35)
+  },
+  avatarContainer: {
+    // position: 'relative',
+    marginTop: px2dp(83),
+    borderRadius: px2dp(70),
+    overflow: 'hidden'
+  },
+  avatar: {
+    width: px2dp(140),
+    height: px2dp(140),
+  },
+  infoLineOne: {
+    position: 'relative',
+    marginTop: px2dp(22)
+  },
+  infoText: {
+    fontSize: px2sp(28),
+    color: '#fff'
+  },
+  sexIcon: {
+    position: 'absolute',
+    top: px2dp(4),
+    right: -px2dp(42)
+  },
+  infoLineTwo: {
+    marginTop: px2dp(18)
+  },
+  infoLineThree: {
+    flexDirection: 'row',
+    marginTop: px2dp(20)
+  },
+  confirmIcon: {
+    width: px2dp(30),
+    height: px2dp(30),
+    marginTop: px2dp(4),
+    marginRight: px2dp(11),
+  },
+  confirm: {
+    fontSize: px2sp(26),
+    color: '#ddd'
+  }
 })
