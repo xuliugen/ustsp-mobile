7f046ffeebaed58c466d08742bb5001f9341e787
zeroyu <zero@zerosrat.com>
2018-07-23 10:42:41 +0800

    feat(msg): 好友请求，右滑item出现“不通过”

--- src/views/messages/FriendRequestScreen.js
+++ src/views/messages/FriendRequestScreen.js
@@ -2,19 +2,31 @@ import React from 'react'
 import { View, StyleSheet } from 'react-native'
 import { withNavigation } from 'react-navigation'
 import { connect } from 'react-redux'
+// https://github.com/dancormier/react-native-swipeout
+import Swipeout from 'react-native-swipeout'
 
 import { APP_BACKGROUD_COLOR } from 'src/styles/common'
-import RequestItem from './components/RequestItem'
 import { px2dp } from 'src/utils/device'
+
+import RequestItem from './components/RequestItem'
 
 @connect()
 @withNavigation
 export default class FriendRequestScreen extends React.Component {
   render() {
+    const swipeoutBtns = [
+      {
+        text: '不通过',
+        backgroundColor: '#fa2024',
+        color: '#fff'
+      }
+    ]
     return (
       <View style={styles.conatiner}>
+        <Swipeout autoClose right={swipeoutBtns}>
+          <RequestItem btnText={'通过'} />
+        </Swipeout>
         <RequestItem passed btnText={'已通过'} />
-        <RequestItem passed={false} btnText={'通过'} />
       </View>
     )
   }
